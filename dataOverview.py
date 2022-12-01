c249420c92b43d5ba60050fe4d24e897de8a3f8b
Akash <akashkumarsingh11032001@gmail.com>
2022-12-01 22:42:23 +0530

    data

--- dataOverview.py
+++ dataOverview.py
@@ -5,28 +5,30 @@ import xlsxwriter
 # data_c = [Size, Block size, IO-DEPTH, Thread]
 
 data = [
-    ['3d61125', '1.02 TB'], 
+    ['3d61125', '1.02 TB'],
     [
-        [23.3, 3060.0, 336.04, 269.0, 310.0, 1369.0, 5276.0, 5473.0, 6390.0, 6456.0], 
-        [27.3, 3576.0, 290.1, 297.0, 322.0, 408.0, 441.0, 465.0, 1074.0, 1106.0], 
-        [6.924, 908.0, 1123.03, 1237.0, 1385.0, 3916.0, 7111.0, 7635.0, 7767.0, 7767.0], 
-        [27.5, 3610.0, 287.41, 297.0, 322.0, 396.0, 498.0, 750.0, 1106.0, 1156.0], 
-        [136.0, 556.0, 232.73, 93.0, 215.0, 1156.0, 17695.0, 20317.0, 27132.0, 27395.0], 
-        [275.0, 1127.0, 114.77, 101.0, 133.0, 273.0, 367.0, 461.0, 832.0, 922.0], 
-        [88.6, 363.0, 10.04978, 10.0, 10.0, 54.0, 127.0, 1811.0, 5735.0, 11207.0], 
+        [23.3, 3060.0, 336.04, 269.0, 310.0, 1369.0, 5276.0, 5473.0, 6390.0, 6456.0],
+        [27.3, 3576.0, 290.1, 297.0, 322.0, 408.0, 441.0, 465.0, 1074.0, 1106.0],
+        [6.924, 908.0, 1123.03, 1237.0, 1385.0,
+            3916.0, 7111.0, 7635.0, 7767.0, 7767.0],
+        [27.5, 3610.0, 287.41, 297.0, 322.0, 396.0, 498.0, 750.0, 1106.0, 1156.0],
+        [136.0, 556.0, 232.73, 93.0, 215.0, 1156.0,
+            17695.0, 20317.0, 27132.0, 27395.0],
+        [275.0, 1127.0, 114.77, 101.0, 133.0, 273.0, 367.0, 461.0, 832.0, 922.0],
+        [88.6, 363.0, 10.04978, 10.0, 10.0, 54.0, 127.0, 1811.0, 5735.0, 11207.0],
         [12.3, 50.4, 80.15, 89.0, 90.0, 93.0, 102.0, 188.0, 594.0, 816.0]
-    ], 
+    ],
     [
-        ['1GB', '128KB', '8', '1'], 
-        ['1GB', '128KB', '8', '1'], 
-        ['100%', '128KB', '8', '1'], 
-        ['100%', '128KB', '8', '1'], 
-        ['1GB', '4KB', '2', '6'], 
-        ['1GB', '4KB', '2', '6'], 
-        ['1GB', '4KB', '1', '1'], 
+        ['1GB', '128KB', '8', '1'],
+        ['1GB', '128KB', '8', '1'],
+        ['100%', '128KB', '8', '1'],
+        ['100%', '128KB', '8', '1'],
+        ['1GB', '4KB', '2', '6'],
+        ['1GB', '4KB', '2', '6'],
+        ['1GB', '4KB', '1', '1'],
         ['1GB', '4KB', '1', '1']
-        ]
     ]
+]
 
 
 workbook = xlsxwriter.Workbook('write_data.xlsx')
@@ -34,7 +36,7 @@ worksheet = workbook.add_worksheet()
 
 # Drive Standard Configuration.
 worksheet.write("A1", "Standard Device Config.")
-worksheet.write("A2", "Firware Rev.")  
+worksheet.write("A2", "Firware Rev.")
 worksheet.write("A3", "Drive Capacity")
 worksheet.write("B2", data[0][0])
 worksheet.write("B3", data[0][1])
@@ -42,12 +44,12 @@ worksheet.write("B3", data[0][1])
 # Standard FIO Scripts
 worksheet.write("A6", "BURST SEQUENTIAL WRITE")
 worksheet.write("A7", "BURST SEQUENTIAL READ")
-worksheet.write("A8", "BURST RANDOM WRITE")
-worksheet.write("A9", "BURST RANDOM READ")
-worksheet.write("A10", "BURST RANDOM WRITE OIO")
-worksheet.write("A11", "BURST RANDOM READ OIO")
-worksheet.write("A12", "SUBSTAIN SEQUENTIAL WRITE")
-worksheet.write("A13", "SUBSTAIN SEQUENTIAL WRITE")
+worksheet.write("A8", "SUBSTAIN SEQUENTIAL WRITE")
+worksheet.write("A9", "SUBSTAIN SEQUENTIAL READ")
+worksheet.write("A10", "BURST RANDOM WRITE")
+worksheet.write("A11", "BURST RANDOM READ")
+worksheet.write("A12", "BURST RANDOM WRITE OIO")
+worksheet.write("A13", "BURST RANDOM READ OIO")
 
 # Table column name
 worksheet.write("B5", "SIZE")
@@ -103,7 +105,77 @@ worksheet.write("N7", data[1][fileIndex][7])
 worksheet.write("O7", data[1][fileIndex][8])
 worksheet.write("P7", data[1][fileIndex][9])
 
+fileIndex = fileIndex + 1
+# SUBSTAIN SEQUENTIAL WRITE
+worksheet.write("B8", data[2][fileIndex][0])
+worksheet.write("C8", data[2][fileIndex][1])
+worksheet.write("D8", data[2][fileIndex][2])
+worksheet.write("E8", data[2][fileIndex][3])
+worksheet.write("F8", "BPC1")
+worksheet.write("G8", data[1][fileIndex][0])
+worksheet.write("H8", data[1][fileIndex][1])
+worksheet.write("I8", data[1][fileIndex][2])
+worksheet.write("J8", data[1][fileIndex][3])
+worksheet.write("K8", data[1][fileIndex][4])
+worksheet.write("L8", data[1][fileIndex][5])
+worksheet.write("M8", data[1][fileIndex][6])
+worksheet.write("N8", data[1][fileIndex][7])
+worksheet.write("O8", data[1][fileIndex][8])
+worksheet.write("P8", data[1][fileIndex][9])
 
+fileIndex = fileIndex + 1
+# SUBSTAIN SEQUENTIAL READ
+worksheet.write("B9", data[2][fileIndex][0])
+worksheet.write("C9", data[2][fileIndex][1])
+worksheet.write("D9", data[2][fileIndex][2])
+worksheet.write("E9", data[2][fileIndex][3])
+worksheet.write("F9", "BPC1")
+worksheet.write("G9", data[1][fileIndex][0])
+worksheet.write("H9", data[1][fileIndex][1])
+worksheet.write("I9", data[1][fileIndex][2])
+worksheet.write("J9", data[1][fileIndex][3])
+worksheet.write("K9", data[1][fileIndex][4])
+worksheet.write("L9", data[1][fileIndex][5])
+worksheet.write("M9", data[1][fileIndex][6])
+worksheet.write("N9", data[1][fileIndex][7])
+worksheet.write("O9", data[1][fileIndex][8])
+worksheet.write("P9", data[1][fileIndex][9])
+
+fileIndex = fileIndex + 1
+# BURST RANDOM WRITE
+worksheet.write("B9", data[2][fileIndex][0])
+worksheet.write("C9", data[2][fileIndex][1])
+worksheet.write("D9", data[2][fileIndex][2])
+worksheet.write("E9", data[2][fileIndex][3])
+worksheet.write("F9", "BPC1")
+worksheet.write("G9", data[1][fileIndex][0])
+worksheet.write("H9", data[1][fileIndex][1])
+worksheet.write("I9", data[1][fileIndex][2])
+worksheet.write("J9", data[1][fileIndex][3])
+worksheet.write("K9", data[1][fileIndex][4])
+worksheet.write("L9", data[1][fileIndex][5])
+worksheet.write("M9", data[1][fileIndex][6])
+worksheet.write("N9", data[1][fileIndex][7])
+worksheet.write("O9", data[1][fileIndex][8])
+worksheet.write("P9", data[1][fileIndex][9])
+
+fileIndex = fileIndex + 1
+# BURST RANDOM READ
+worksheet.write("B10", data[2][fileIndex][0])
+worksheet.write("C10", data[2][fileIndex][1])
+worksheet.write("D10", data[2][fileIndex][2])
+worksheet.write("E10", data[2][fileIndex][3])
+worksheet.write("F10", "BPC1")
+worksheet.write("G10", data[1][fileIndex][0])
+worksheet.write("H10", data[1][fileIndex][1])
+worksheet.write("I10", data[1][fileIndex][2])
+worksheet.write("J10", data[1][fileIndex][3])
+worksheet.write("K10", data[1][fileIndex][4])
+worksheet.write("L10", data[1][fileIndex][5])
+worksheet.write("M10", data[1][fileIndex][6])
+worksheet.write("N10", data[1][fileIndex][7])
+worksheet.write("O10", data[1][fileIndex][8])
+worksheet.write("P10", data[1][fileIndex][9])
 
 
 workbook.close()
