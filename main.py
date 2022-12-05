be01935502d27b167d385315ec32e0049523f9c7
Akash <akashkumarsingh11032001@gmail.com>
2022-12-05 09:28:54 +0530

    ./

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ from scriptInfo import infoScriptEntry
 from formatDrive import driveFormat
 from dataOverview import excelPlot
 
-bpc_1 = "preCondition\\bpc1.txt"
 
 fio = [
     "fioScripts\\burst_seqwr.txt",
@@ -40,67 +39,70 @@ passd = "12"
 
 dirx = "/root/fio/"
 
+
+
+def FIOexecution(server, user, passd, dirx, fio):
+    # <------------------> BPC1 ------------------>
+    bpc_1 = "preCondition\\bpc1.txt"
+    bpc1(server, user, passd, dirx, bpc_1)
+    status("<--------- Precondition BPC1 is been implemented! --------->", 1)
+
+    # <<< DRIVE FORMAT >>>
+    driveFormat(server, user, passd, dirx)
+    status("FORMAT DONE!", 0.5)
+
+    # <--------- BURST SEQUENTIAL WRITE --------->
+    burst_seqwr(server, user, passd, dirx, fio[0])
+    status("<--------- BURST SEQUENTIAL WRITE is been implemented! --------->", 1)
+
+    # <--------- BURST SEQUENTIAL READ --------->
+    burst_seqrd(server, user, passd, dirx, fio[1])
+    status("<--------- BURST SEQUENTIAL READ is been implemented! --------->", 1)
+
+    # <<< DRIVE FORMAT >>>
+    driveFormat(server, user, passd, dirx)
+    status("FORMAT DONE!", 0.5)
+
+    # <--------- SUSTAINED SEQUENTIAL WRITE --------->
+    sus_seqwr(server, user, passd, dirx, fio[2])
+    status("<--------- SUSTAINED SEQUENTIAL WRITE is been implemented! --------->", 1)
+
+    # <--------- SUSTAINED SEQUENTIAL READ --------->
+    sus_seqrd(server, user, passd, dirx, fio[3])
+    status("<--------- SUSTAINED SEQUENTIAL READ is been implemented! --------->", 1)
+
+    # <<< DRIVE FORMAT >>>
+    driveFormat(server, user, passd, dirx)
+    status("FORMAT DONE!", 0.5)
+
+    # <--------- BURST RANDOM WRITE --------->
+    burst_randwr(server, user, passd, dirx, fio[4])
+    status("<--------- BURST RANDOM WRITE is been implemented! --------->", 1)
+
+    # <--------- BURST RANDOM READ --------->
+    burst_randrd(server, user, passd, dirx, fio[5])
+    status("<--------- BURST RANDOM READ is been implemented! --------->", 1)
+
+    # <<< DRIVE FORMAT >>>
+    driveFormat(server, user, passd, dirx)
+    status("FORMAT DONE!", 0.5)
+
+
+    # <--------- BURST RANDOM WRITE OIO --------->
+    burst_randwr_oio(server, user, passd, dirx, fio[6])
+    status("<--------- BURST RANDOM WRITE OIO is been implemented! --------->", 1)
+
+    # <--------- BURST RANDOM READ OIO --------->
+    burst_randrd_oio(server, user, passd, dirx, fio[7])
+    status("<--------- BURST RANDOM READ OIO is been implemented! --------->", 1)
+    
 # ............................. <<< Controler Identify >>>  ............................. #
 identifyCtrl(server, user, passd, dirx)
 data_a = parseControlerData()  # single list data
 status("<--------- Controler Identify Data Structure is been implemented! --------->", 1)
 
-
 # ............................. <<< FIO SCRIPT EXECUTION >>>............................. #
-
-# <------------------> BPC1 ------------------>
-bpc1(server, user, passd, dirx, bpc_1)
-status("<--------- Precondition BPC1 is been implemented! --------->", 1)
-
-# <<< DRIVE FORMAT >>>
-driveFormat(server, user, passd, dirx)
-status("FORMAT DONE!", 0.5)
-
-# <--------- BURST SEQUENTIAL WRITE --------->
-burst_seqwr(server, user, passd, dirx, fio[0])
-status("<--------- BURST SEQUENTIAL WRITE is been implemented! --------->", 1)
-
-# <--------- BURST SEQUENTIAL READ --------->
-burst_seqrd(server, user, passd, dirx, fio[1])
-status("<--------- BURST SEQUENTIAL READ is been implemented! --------->", 1)
-
-# <<< DRIVE FORMAT >>>
-driveFormat(server, user, passd, dirx)
-status("FORMAT DONE!", 0.5)
-
-# <--------- SUSTAINED SEQUENTIAL WRITE --------->
-sus_seqwr(server, user, passd, dirx, fio[2])
-status("<--------- SUSTAINED SEQUENTIAL WRITE is been implemented! --------->", 1)
-
-# <--------- SUSTAINED SEQUENTIAL READ --------->
-sus_seqrd(server, user, passd, dirx, fio[3])
-status("<--------- SUSTAINED SEQUENTIAL READ is been implemented! --------->", 1)
-
-# <<< DRIVE FORMAT >>>
-driveFormat(server, user, passd, dirx)
-status("FORMAT DONE!", 0.5)
-
-# <--------- BURST RANDOM WRITE --------->
-burst_randwr(server, user, passd, dirx, fio[4])
-status("<--------- BURST RANDOM WRITE is been implemented! --------->", 1)
-
-# <--------- BURST RANDOM READ --------->
-burst_randrd(server, user, passd, dirx, fio[5])
-status("<--------- BURST RANDOM READ is been implemented! --------->", 1)
-
-# <<< DRIVE FORMAT >>>
-driveFormat(server, user, passd, dirx)
-status("FORMAT DONE!", 0.5)
-
-
-# <--------- BURST RANDOM WRITE OIO --------->
-burst_randwr_oio(server, user, passd, dirx, fio[6])
-status("<--------- BURST RANDOM WRITE OIO is been implemented! --------->", 1)
-
-# <--------- BURST RANDOM READ OIO --------->
-burst_randrd_oio(server, user, passd, dirx, fio[7])
-status("<--------- BURST RANDOM READ OIO is been implemented! --------->", 1)
-
+FIOexecution(server, user, passd, dirx, fio)
 
 # ............................. <<< PERFORMANCE ENTRY DATA COLLECTION >>> ............................. #
 data_b = performanceEntry()  # list of list data
@@ -115,8 +117,7 @@ status("<--------- Script Entry Data Completed! --------->", 1)
 # data_b = [Iops, Bandwidth, AVg. latecy, 50th-99.9999th ]
 # data_c = [Size, Block size, IO-DEPTH, Thread]
 
-print("a : {0}, b : {1}, c : {2}".format(
-    len(data_a), len(data_b), len(data_c)))
+
 final = [data_a, data_b, data_c]
 # print(final)
 excelPlot(final)
