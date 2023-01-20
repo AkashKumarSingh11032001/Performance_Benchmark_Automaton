ce21963989d9825e0d70dff22ab22ca9a3a8d42b
Akash <akashkumarsingh11032001@gmail.com>
2023-01-20 13:51:31 +0530

    ./

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ from hmb import HmbOnOff
 from conectionBetweenTwoHost import HostsConnectionandCommandExecution
 from conectionBetweenTwoHost import HostsFileTransfer
 from os.path import dirname
+from BPC1 import bpc1
 
-# x= dirname(__file__)
+localPath = dirname(__file__)
 # print("{}\\burst.txt".format(x))
 
 user_server = input("\nPlease Enter Ip address: ")
@@ -23,8 +24,11 @@ print("10 - Only (BPC1 + Fios) For HMB OFF\n")
 Opr = input("Select Opration you want to Perform on your drive: ")
 
 if Opr == "1":
-    pass
-elif Opr == "3":
+    bpc_1 = "{0}\\preCondition\\bpc1.txt".format(localPath)
+    bpc1(user_server, "root", "12", "/root/fio/", bpc_1)
+    status("<--------- Precondition BPC1 is been implemented! --------->", 1)
+
+elif Opr == "2":
     # <<<<< HMB ON >>>>>
     print("\n|||||||||||||||||||||\n")
     print(" \tHMB ON\t ")
@@ -55,37 +59,147 @@ elif Opr == "3":
 
     print("\n ************ Task Accomplished ************ ")
 
-elif Opr == "2":
-    print("\n<<<<<<<< BPC0 Triggered! >>>>>>>>\n")
-    ExeBPC0(user_server, "")
-    print("\n ************ Task Accomplished ************ ")
 elif Opr == "3":
+    # <<<<< HMB ON >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB ON\t ")
+    print("\n|||||||||||||||||||||\n")
+
+    print("\n<<<<<<<< BPC0 Triggered! >>>>>>>>\n")
+    ExeBPC0(user_server, "HMB_ON")
     print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
-    ExeBPC1(user_server, "")
+    ExeBPC1(user_server, "HMB_ON")
     print("\n ************ Task Accomplished ************ ")
 elif Opr == "4":
+    # <<<<< HMB Condition >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tTurning HMB ON -> HMB OFF\t")
+    print("\n|||||||||||||||||||||\n")
+
+    HmbOnOff(user_server, "root", "12", "/root/fio/")
+
+    # <<<<< HMB OFF >>>>>
+    # print("\n||||||||||||||||||||| HMB OFF |||||||||||||||||||||\n")
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB OFF\t ")
+    print("\n|||||||||||||||||||||\n")
+
+    print("\n<<<<<<<< BPC0 Triggered! >>>>>>>>\n")
+    ExeBPC0(user_server, "HMB_OFF")
     print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
-    ExeBPC1(user_server, "")
+    ExeBPC1(user_server, "HMB_OFF")
     print("\n ************ Task Accomplished ************ ")
+    
 elif Opr == "5":
-    print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
-    ExeBPC1(user_server, "")
+    # <<<<< HMB ON >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB ON\t ")
+    print("\n|||||||||||||||||||||\n")
+
+    print("\n<<<<<<<< BPC0 Triggered! >>>>>>>>\n")
+    ExeBPC0(user_server, "HMB_ON")
+    
+
+    # <<<<< HMB Condition >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tTurning HMB ON -> HMB OFF\t")
+    print("\n|||||||||||||||||||||\n")
+
+    HmbOnOff(user_server, "root", "12", "/root/fio/")
+
+    # <<<<< HMB OFF >>>>>
+    # print("\n||||||||||||||||||||| HMB OFF |||||||||||||||||||||\n")
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB OFF\t ")
+    print("\n|||||||||||||||||||||\n")
+
+    print("\n<<<<<<<< BPC0 Triggered! >>>>>>>>\n")
+    ExeBPC0(user_server, "HMB_OFF")
+
     print("\n ************ Task Accomplished ************ ")
 elif Opr == "6":
+    # <<<<< HMB ON >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB ON\t ")
+    print("\n|||||||||||||||||||||\n")
+
     print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
-    ExeBPC1(user_server, "")
+    ExeBPC1(user_server, "HMB_ON")
+
+    # <<<<< HMB Condition >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tTurning HMB ON -> HMB OFF\t")
+    print("\n|||||||||||||||||||||\n")
+
+    HmbOnOff(user_server, "root", "12", "/root/fio/")
+
+    # <<<<< HMB OFF >>>>>
+    # print("\n||||||||||||||||||||| HMB OFF |||||||||||||||||||||\n")
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB OFF\t ")
+    print("\n|||||||||||||||||||||\n")
+
+    print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
+    ExeBPC1(user_server, "HMB_OFF")
+
     print("\n ************ Task Accomplished ************ ")
 elif Opr == "7":
-    print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
-    ExeBPC1(user_server, "")
+    # <<<<< HMB ON >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB ON\t ")
+    print("\n|||||||||||||||||||||\n")
+
+    print("\n<<<<<<<< BPC0 Triggered! >>>>>>>>\n")
+    ExeBPC0(user_server, "HMB_ON")
+
     print("\n ************ Task Accomplished ************ ")
 elif Opr == "8":
+    # <<<<< HMB ON >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB ON\t ")
+    print("\n|||||||||||||||||||||\n")
+
     print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
-    ExeBPC1(user_server, "")
+    ExeBPC1(user_server, "HMB_ON")
+
     print("\n ************ Task Accomplished ************ ")
 elif Opr == "9":
+
+    # <<<<< HMB Condition >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tTurning HMB ON -> HMB OFF\t")
+    print("\n|||||||||||||||||||||\n")
+
+    HmbOnOff(user_server, "root", "12", "/root/fio/")
+
+    # <<<<< HMB OFF >>>>>
+    # print("\n||||||||||||||||||||| HMB OFF |||||||||||||||||||||\n")
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB OFF\t ")
+    print("\n|||||||||||||||||||||\n")
+
+    print("\n<<<<<<<< BPC0 Triggered! >>>>>>>>\n")
+    ExeBPC0(user_server, "HMB_OFF")
+
+    print("\n ************ Task Accomplished ************ ")
+elif Opr == "10":
+
+    # <<<<< HMB Condition >>>>>
+    print("\n|||||||||||||||||||||\n")
+    print(" \tTurning HMB ON -> HMB OFF\t")
+    print("\n|||||||||||||||||||||\n")
+
+    HmbOnOff(user_server, "root", "12", "/root/fio/")
+
+    # <<<<< HMB OFF >>>>>
+    # print("\n||||||||||||||||||||| HMB OFF |||||||||||||||||||||\n")
+    print("\n|||||||||||||||||||||\n")
+    print(" \tHMB OFF\t ")
+    print("\n|||||||||||||||||||||\n")
+
     print("\n<<<<<<<< BPC1 Triggered! >>>>>>>>\n")
-    ExeBPC1(user_server, "")
+    ExeBPC1(user_server, "HMB_OFF")
+
     print("\n ************ Task Accomplished ************ ")
 else:
     print("Please choose Appropiate option!")
